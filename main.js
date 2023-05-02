0d5dbab2abe70f9a152554f3a7270025134d46a0
kyanysh <kyanyshbd30@gmail.com>
2023-05-02 15:59:44 +0300

    dop lesson 2

--- main.js
+++ main.js
@@ -1,63 +1,24 @@
-// Triangle
+//Второе задание (Дополнительное)
 
-var triangle = "#";
+//6 задача
 
-do {
-    console.log(triangle);
-    triangle += "#";
-} while (triangle.length <= 7);
+let arr1 = [1, true, false, undefined];
+let arr2 = ['Masha', 'Sabina', false, undefined];
 
-// Triangle
-
-// FizzBuzz
-
-for (var i = 1; i <= 100; i++) {
-    if (!(i % 3) && !(i % 5)) {
-        console.log(i, "FizzBuzz");
-    } else if (!(i % 3)) {
-        console.log(i, "Fizz");
-    } else if (!(i % 5)) {
-        console.log(i, "Buzz");
+for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] !== undefined) {
+        arr1[i] = arr2[i];
     }
 }
 
-// FizzBuzz
+console.log(arr1);
 
-// шахматная доска
+//5 задача
 
-var deskSize = 8,
-    totalSize = deskSize * deskSize,
-    line = "",
-    flag = true;
+let array = ['Алина', 'Паша', 'Миша'];
+console.log(array[0]);
 
+//4 Задача
 
-function createLine(deskSize, flag) {
-    var result = "";
-    for (var i = 0; i < deskSize; i++) {
-        (i % 2) ? result += "#": result += " ";
-    }
-    if (flag) {
-        result = reverseString(result);
-    }
-    return result;
-}
-
-function reverseString(str) {
-    var result = "";
-    for (var i = str.length - 1; i > 0; i--) {
-        result += str[i];
-    }
-    return result;
-}
-
-
-for (var i = 1; i <= deskSize; i++) {
-    if (flag) {
-        flag = false;
-    } else {
-        flag = true;
-    }
-    console.log(createLine(deskSize, flag));
-}
-
-//шахматная доска
+const mass = ['Alex', 'Anna', 'Oleg', '1', '2', '3', true]
+console.log(mass);
